3a7cda6766d45086b2e61113ec49675d9d98c147
박상희 (Alex Park) <psh010209@gmail.com>
2022-01-22 17:37:00 +0900

    chore: no cache

--- loader.sh
+++ loader.sh
@@ -3,7 +3,7 @@
 rm -rf $HOME/.devifier
 
 mkdir -p $HOME/.devifier
-curl https://raw.githubusercontent.com/Alex4386/devifier/main/devifier.sh > $HOME/.devifier/devifier.sh
+curl  -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/Alex4386/devifier/main/devifier.sh > $HOME/.devifier/devifier.sh
 chmod +x $HOME/.devifier/devifier.sh
 
 $HOME/.devifier/devifier.sh
