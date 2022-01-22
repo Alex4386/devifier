2ff1009d0bb6c8df00bc6e8ce343233c804819a3
박상희 (Alex Park) <psh010209@gmail.com>
2022-01-22 17:36:02 +0900

    fix: unlimited bash thing

--- loader.sh
+++ loader.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+rm -rf $HOME/.devifier
+
 mkdir -p $HOME/.devifier
 curl https://raw.githubusercontent.com/Alex4386/devifier/main/devifier.sh > $HOME/.devifier/devifier.sh
 chmod +x $HOME/.devifier/devifier.sh
 
-bash $HOME/.devifier/devifier.sh
+$HOME/.devifier/devifier.sh
